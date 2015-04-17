4d7b44e0c852b1c678b3abb0de52e60c2bd5784d
sanjsanj <sanjsanj@hotmail.com>
2015-04-17 19:36:31 +0100

    Bike can be broken and fixed

--- spec/BikeSpec.js
+++ spec/BikeSpec.js
@@ -6,12 +6,29 @@ describe('Bike Unit Test', function() {
     bike = new Bike
   });
 
-  describe('New bike is', function() {
+  describe('New bike is', function(){
 
     it('not broken',function(){
       expect(bike.isBroken()).toBe(false);
     });
+  });
 
+  describe('Bike can', function(){
+
+    it('be broken', function(){
+      bike.break();
+      expect(bike.isBroken()).toBe(true);
+    });
+
+    it('be fixed if broken', function(){
+      bike.break();
+      bike.fix();
+      expect(bike.isBroken()).toBe(false);
+    });
+
+    it('not be fixed if not broken', function(){
+      expect(function(){bike.fix()}).toThrow('Bike not broken');
+    });
   });
 
 });
--- src/Bike.js
+++ src/Bike.js
@@ -5,3 +5,15 @@ var Bike = function(){
 Bike.prototype.isBroken = function(){
   return this._isBroken;
 };
+
+Bike.prototype.break = function(){
+  this._isBroken = true;
+};
+
+Bike.prototype.fix = function(){
+  if (this._isBroken === true){
+    this._isBroken = false;
+  } else {
+    throw 'Bike not broken';
+  };
+};
